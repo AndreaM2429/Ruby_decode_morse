e8debf38f4b4eea9b9157bf7c41816835d28f447
HFG43 <hernanguemes@hotmail.com>
2023-08-23 19:00:45 -0300

    Misc: correct Linters

--- decode.rb
+++ decode.rb
@@ -1,7 +1,7 @@
 MORSE_CODE = {
   '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D',
   '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
-  '..' => 'I', '.---' => 'J', '-.-' => 'K',   '.-..' => 'L',
+  '..' => 'I', '.---' => 'J', '-.-' => 'K', '.-..' => 'L',
   '--' => 'M', '-.' => 'N', '---' => 'O', '.--.' => 'P',
   '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
   '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X',
@@ -9,25 +9,24 @@ MORSE_CODE = {
   '-----' => '0', '.----' => '1', '..---' => '2', '...--' => '3',
   '....-' => '4', '.....' => '5', '-....' => '6', '--...' => '7',
   '---..' => '8', '----.' => '9'
-}
+}.freeze
 
-def decode_char (char)
+def decode_char(char)
   MORSE_CODE[char]
 end
 
-def decode_word (word)
-  leters = word.split
-  leters_decode = leters.map do |leter|
-    leter = decode_char(leter)
+def decode_word(word)
+  word.split.map do |leter|
+    decode_char(leter)
   end
 end
 
-def decode_message (message)
+def decode_message(message)
   words = message.split('   ')
   words_decode = words.map do |word|
-    word = decode_word(word).join('')
+    decode_word(word).join('')
   end
-  words_decode.each {|word| print "#{word} "}
+  words_decode.each { |word| print "#{word} " }
 end
 
 decode_message '.-   -... --- -..-   ..-. ..- .-.. .-..   --- ..-.   .-. ..- -... .. . ...'
